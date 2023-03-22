c70ae4a771e11093aaf84bae456b44f89c359af2
Alfred Boateng <programmingwithalfred@gmail.com>
2023-03-22 11:16:03 +0000

    Seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,37 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
-#   Character.create(name: "Luke", movie: movies.first)
+10.times do
+  Language.create([
+    { 
+      name: Faker::ProgrammingLanguage.name
+    }
+  ])
+end
+
+
+50.times do
+
+Course.create([{
+	name: Faker::Educator.course_name,
+	description: Faker::Lorem.paragraph(sentence_count: 20),
+	language_id: Language.all.sample.id,
+	category_id: Category.all.sample.id,
+
+}])
+	end
+
+300.times do
+  Section.create!([{
+    name: Faker::Educator.subject,
+    description: Faker::Lorem.paragraph(sentence_count: 2),
+    course_id: Course.all.sample.id,
+  }])
+end
+
+
+400.times do
+  Lecture.create!([{
+    name: Faker::Educator.course_name,
+    url: Faker::Internet.url,
+    description: Faker::Lorem.paragraph(sentence_count: 2),
+    section_id: Section.all.sample.id,
+  }])
+end
