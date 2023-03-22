71092069ba0bf256fb517103891411cd8184b058
Alfred Boateng <programmingwithalfred@gmail.com>
2023-03-22 06:04:56 +0000

    add language entity

---
+++ app/controllers/homes_controller.rb
@@ -0,0 +1,9 @@
+class HomesController < ApplicationController
+  def index
+  end
+  
+
+  def about
+  end
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,5 @@
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  get '/about', to: 'homes#about', as: 'about'
 
-  # Defines the root path route ("/")
-  # root "articles#index"
+  root "homes#index"
 end
