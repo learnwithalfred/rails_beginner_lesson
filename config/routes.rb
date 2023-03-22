ffdc1ced0023f92b9d283ddfccd01d1ff747868a
Alfred Boateng <programmingwithalfred@gmail.com>
2023-03-22 10:17:09 +0000

    Category

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,14 @@
 Rails.application.routes.draw do
-  get '/languages', to: 'languages#index', as: 'languages'
-  get '/languages/new', to: 'languages#new', as: 'new_language'
-  post '/languages', to: 'languages#create', as: 'create_language'
-  get '/languages/:id', to: 'languages#show', as: 'language'
-  get '/about', to: 'homes#about', as: 'about'
-  get '/languages/:id/edit', to: 'languages#edit', as: 'edit_language'
-  patch '/languages/:id', to: 'languages#update', as: 'update_language'
-  delete '/languages/:id', to: 'languages#destroy', as: 'destroy_language'
+  resources :categories
+  resources :languages
+
+  # get '/languages', to: 'languages#index', as: 'languages'
+  # get '/languages/new', to: 'languages#new', as: 'new_language'
+  # post '/languages', to: 'languages#create', as: 'create_language'
+  # get '/languages/:id', to: 'languages#show', as: 'language'
+  # get '/about', to: 'homes#about', as: 'about'
+  # get '/languages/:id/edit', to: 'languages#edit', as: 'edit_language'
+  # patch '/languages/:id', to: 'languages#update', as: 'update_language'
+  # delete '/languages/:id', to: 'languages#destroy', as: 'destroy_language'
   root "homes#index"
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_03_22_055546) do
+ActiveRecord::Schema[7.0].define(version: 2023_03_22_101053) do
+  create_table "categories", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
   create_table "languages", force: :cascade do |t|
     t.string "name"
     t.datetime "created_at", null: false
