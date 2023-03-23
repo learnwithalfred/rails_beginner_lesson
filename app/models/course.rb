3c72b91cedf81c32f97d7012a5ef610f8b9edb61
Alfred Boateng <programmingwithalfred@gmail.com>
2023-03-23 18:59:17 +0000

    Course test

--- app/models/course.rb
+++ app/models/course.rb
@@ -1,4 +1,7 @@
 class Course < ApplicationRecord
+  validates :name, presence: true, length: {minimum:3, maximum:255}
+    validates :description, presence: true, length: {minimum:3}
+
   belongs_to :language
   belongs_to :category
   has_many :sections
